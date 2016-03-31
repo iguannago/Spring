251553f66d5303f4950d42f80bd14b60516f3f4a
David Crespo <davidcrespoarroyo@gmail.com>
2016-03-31 19:07:33 +0100

    Implemented the Application code to do the call using the RestTemplate

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,10 +1,27 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * Created by davicres on 31/03/2016.
  */
-public class Application {
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        System.out.println("this is the main method");
+        SpringApplication.run(Application.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
     }
 }
