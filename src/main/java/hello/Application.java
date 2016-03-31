63dfa8e1b0f4ea35be0d3d3e1e81f0af79310ed2
David Crespo <davidcrespoarroyo@gmail.com>
2016-03-31 15:38:32 +0100

    Added the Applicaton class which contains the main method.

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by davicres on 31/03/2016.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("this is the main method");
+    }
+}
