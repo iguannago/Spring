07fa89bf01ea2b73935f3554195508265d421adf
David Crespo <davidcrespoarroyo@gmail.com>
2016-11-21 16:19:28 +0000

    Added EvidenceShare class and a simple endpoint

---
+++ springBoot3/evidenceServiceBackend/src/main/java/com/get_in_the_cloud/backend/EvidenceShare.java
@@ -0,0 +1,20 @@
+package com.get_in_the_cloud.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class EvidenceShare {
+
+    @RequestMapping("/")
+    String home() {
+        return "Evidence Share API";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvidenceShare.class, args);
+    }
+}
--- springBoot3/evidenceServiceBackend/src/test/java/com/get_in_the_cloud/backend/EvidenceShareTest.java
+++ springBoot3/evidenceServiceBackend/src/test/java/com/get_in_the_cloud/backend/EvidenceShareTest.java
@@ -3,6 +3,6 @@ package com.get_in_the_cloud.backend;
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+public class EvidenceShareTest {
 
 }
