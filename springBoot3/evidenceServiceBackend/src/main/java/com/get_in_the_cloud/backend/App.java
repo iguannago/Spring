26026996131120d8c87fe081299210a539e20fcc
David Crespo <davidcrespoarroyo@gmail.com>
2016-11-21 15:33:33 +0000

    Created a Spring Boot java project as a placeholder for the backend services.

---
+++ springBoot3/evidenceServiceBackend/src/main/java/com/get_in_the_cloud/backend/App.java
@@ -0,0 +1,13 @@
+package com.get_in_the_cloud.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
