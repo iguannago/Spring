3b590ce6ae6619df8c0be3fc7726d780556c4d98
David Crespo <davidcrespoarroyo@gmail.com>
2016-11-22 15:07:36 +0000

    Added test case for the ID field. Added production code to pass it.

--- springBoot3/evidenceServiceBackend/src/main/java/com/get_in_the_cloud/restServices/Evidence.java
+++ springBoot3/evidenceServiceBackend/src/main/java/com/get_in_the_cloud/restServices/Evidence.java
@@ -4,11 +4,18 @@ package com.get_in_the_cloud.restServices;
  * Created by davicres on 22/11/2016.
  */
 public final class Evidence {
-    private Evidence(String id, String content) {
+    private final String id;
 
+    private Evidence(String id, String content) {
+        this.id = id;
     }
 
     public static Evidence builder(String id, String content) {
         return new Evidence(id, content);
     }
+
+    public String getId() {
+        return id;
+    }
+
 }
--- springBoot3/evidenceServiceBackend/src/test/java/com/get_in_the_cloud/EvidenceTest.java
+++ springBoot3/evidenceServiceBackend/src/test/java/com/get_in_the_cloud/EvidenceTest.java
@@ -3,6 +3,7 @@ package com.get_in_the_cloud;
 import com.get_in_the_cloud.restServices.Evidence;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -12,5 +13,11 @@ public class EvidenceTest {
     @Test
     public void evidenceConstructorTest() {
         assertNotNull(Evidence.builder("E0001", "Appeal to the Social Security and Child Support Tribunal"));
+    }
+
+    @Test
+    public void evidenceFieldsTest() {
+        Evidence evidence = Evidence.builder("E0001", "Appeal to the Social Security and Child Support Tribunal");
+        assertEquals("E0001", evidence.getId());
     }
 }
