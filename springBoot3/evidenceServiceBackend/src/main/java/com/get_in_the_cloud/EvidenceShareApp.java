7d1aee6e9c552e72c5462fce7dcabe747b90acd8
David Crespo <davidcrespoarroyo@gmail.com>
2016-11-22 16:04:11 +0000

    Fixed service is not found because the App classes have to be at the root package level.

--- springBoot3/evidenceServiceBackend/src/main/java/com/get_in_the_cloud/EvidenceShareApp.java
+++ springBoot3/evidenceServiceBackend/src/main/java/com/get_in_the_cloud/EvidenceShareApp.java
@@ -1,4 +1,4 @@
-package com.get_in_the_cloud.app;
+package com.get_in_the_cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
