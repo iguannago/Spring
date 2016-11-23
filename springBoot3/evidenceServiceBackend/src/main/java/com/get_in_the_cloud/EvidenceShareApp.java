7a95a2bd434370fa58779ac822c63f7847a142e9
David Crespo <davidcrespoarroyo@gmail.com>
2016-11-23 14:53:37 +0000

    Configures swagger library to get my rest api nicely listed in a simple UI

--- springBoot3/evidenceServiceBackend/src/main/java/com/get_in_the_cloud/EvidenceShareApp.java
+++ springBoot3/evidenceServiceBackend/src/main/java/com/get_in_the_cloud/EvidenceShareApp.java
@@ -2,11 +2,13 @@ package com.get_in_the_cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by davicres on 22/11/2016.
  */
 @SpringBootApplication
+@EnableSwagger2
 public class EvidenceShareApp {
 
     public static void main(String[] args) {
--- springBoot3/evidenceServiceBackend/src/main/java/com/get_in_the_cloud/restServices/EvidenceShareController.java
+++ springBoot3/evidenceServiceBackend/src/main/java/com/get_in_the_cloud/restServices/EvidenceShareController.java
@@ -1,5 +1,9 @@
 package com.get_in_the_cloud.restServices;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/EvidenceShareService/evidences")
+@Api(value = "/evidences", description = "Evidences REST API")
 public class EvidenceShareController {
 
     @GetMapping("/get/{evidenceId}")
+    @ApiOperation(value = "Get an Evidence", notes = "Get an Evidence given an ID")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success", response = Evidence.class)
+    })
     public Evidence getEvidenceById(@PathVariable String evidenceId) {
         return Evidence.builder(evidenceId, "Appeal to the Social Security and Child Support Tribunal");
     }
