f6e2af3e9d27f375a23c27596be0d046d2312706
David Crespo <davidcrespoarroyo@gmail.com>
2016-12-01 16:46:32 +0000

    Added RestTemplate bean for IoC

---
+++ springBoot3/evidenceServiceBackend/src/main/java/com/get_in_the_cloud/api/elasticSearchImpl/RestTemplateConfig.java
@@ -0,0 +1,16 @@
+package com.get_in_the_cloud.api.elasticSearchImpl;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by davicres on 01/12/2016.
+ */
+@Configurable
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
