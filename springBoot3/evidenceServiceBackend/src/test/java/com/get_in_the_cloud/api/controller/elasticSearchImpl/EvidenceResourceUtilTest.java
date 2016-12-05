a142263390179e59d5397897cdbe5f163d208081
David Crespo <davidcrespoarroyo@gmail.com>
2016-12-05 17:44:51 +0000

    Refactored testing code from the EvidenceResourceUtil class

--- springBoot3/evidenceServiceBackend/src/test/java/com/get_in_the_cloud/api/controller/elasticSearchImpl/EvidenceResourceUtilTest.java
+++ springBoot3/evidenceServiceBackend/src/test/java/com/get_in_the_cloud/api/controller/elasticSearchImpl/EvidenceResourceUtilTest.java
@@ -1,8 +1,10 @@
 package com.get_in_the_cloud.api.controller.elasticSearchImpl;
 
 import com.get_in_the_cloud.api.domain.evidence.Evidence;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -21,37 +23,30 @@ import static org.mockito.Mockito.when;
 @PrepareForTest({ControllerLinkBuilder.class})
 public class EvidenceResourceUtilTest {
 
+    private final Evidence evidence = new Evidence("1", "some content");
+    @Mock
+    private EvidenceSharingElasticSearchController controllerMock;
+    @Mock
+    private ControllerLinkBuilder controllerLinkBuilderMock;
+
+    @Before
+    public void setUp() throws Exception {
+        PowerMockito.mockStatic(ControllerLinkBuilder.class);
+        when(ControllerLinkBuilder.methodOn(eq(EvidenceSharingElasticSearchController.class))).thenReturn(controllerMock);
+    }
+
     @Test
     public void testEvidenceToGETResource() throws Exception {
-        Evidence evidence = new Evidence("1", "some content");
-        PowerMockito.mockStatic(ControllerLinkBuilder.class);
-
-        EvidenceSharingElasticSearchController controllerMock = mock(EvidenceSharingElasticSearchController.class);
-        when(ControllerLinkBuilder.methodOn(eq(EvidenceSharingElasticSearchController.class))).
-                thenReturn(controllerMock);
-
-        ControllerLinkBuilder controllerLinkBuilderMock = mock(ControllerLinkBuilder.class);
         when(ControllerLinkBuilder.linkTo(controllerMock.getEvidenceById("1"))).thenReturn(controllerLinkBuilderMock);
-
         when(controllerLinkBuilderMock.slash("1")).thenReturn(controllerLinkBuilderMock);
         when(controllerLinkBuilderMock.withSelfRel()).thenReturn(mock(Link.class));
-
         Resource<Evidence> evidenceResource = EvidenceResourceUtil.evidenceToGETResource(evidence);
         System.out.println(evidenceResource);
     }
 
     @Test
     public void testEvidenceToPOSTResource() throws Exception {
-        Evidence evidence = new Evidence("1", "some content");
-        PowerMockito.mockStatic(ControllerLinkBuilder.class);
-
-        EvidenceSharingElasticSearchController controllerMock = mock(EvidenceSharingElasticSearchController.class);
-        when(ControllerLinkBuilder.methodOn(eq(EvidenceSharingElasticSearchController.class))).
-                thenReturn(controllerMock);
-
-        ControllerLinkBuilder controllerLinkBuilderMock = mock(ControllerLinkBuilder.class);
         when(ControllerLinkBuilder.linkTo(controllerMock.createEvidence(evidence))).thenReturn(controllerLinkBuilderMock);
-
         when(controllerLinkBuilderMock.withSelfRel()).thenReturn(mock(Link.class));
         Resource<Evidence> evidenceResource = EvidenceResourceUtil.evidenceToPOSTResource(evidence);
         System.out.println(evidenceResource);
