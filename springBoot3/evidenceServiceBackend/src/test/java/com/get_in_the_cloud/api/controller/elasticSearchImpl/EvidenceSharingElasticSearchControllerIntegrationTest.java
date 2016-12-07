0a0d5e4815bbe1ec14f95aa9b92019bd3c50fbab
David Crespo <davidcrespoarroyo@gmail.com>
2016-12-07 14:41:36 +0000

    Configured the GET evidence integration test.

--- springBoot3/evidenceServiceBackend/src/test/java/com/get_in_the_cloud/api/controller/elasticSearchImpl/EvidenceSharingElasticSearchControllerIntegrationTest.java
+++ springBoot3/evidenceServiceBackend/src/test/java/com/get_in_the_cloud/api/controller/elasticSearchImpl/EvidenceSharingElasticSearchControllerIntegrationTest.java
@@ -1,13 +1,12 @@
 package com.get_in_the_cloud.api.controller.elasticSearchImpl;
 
 import com.get_in_the_cloud.api.domain.evidence.Evidence;
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
 import org.junit.After;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,34 +16,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by davicres on 23/11/2016.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class EvidenceSharingElasticSearchControllerIntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
-    private final Node node = new NodeBuilder().node().start();
+    private final Node node = new NodeBuilder().local(true).node().start();
     private final Client client = node.client();
 
-    @Test@Ignore
-    public void getEvidenceEndpointTest() {
-        CreateIndexRequest indexRequest = new CreateIndexRequest("evidences");
-        client.admin().indices().create(indexRequest).actionGet();
-
-        client.admin().indices().prepareTypesExists("evidences").setTypes("evidences").execute();
-
-//        PutMappingResponse response = client.admin().indices().preparePutMapping(schemaName).setSource(mapping)
-//                .setType("evidence").execute().actionGet();
-
-
+    @Test
+    public void getEvidenceEndpointTest() throws InterruptedException {
+        IndexRequestBuilder indexRequestBuilder = client.prepareIndex("evidences", "evidence", "E001");
+        Map<String, Object> source = new HashMap<>(1);
+        source.put("evidenceID", "E001");
+        source.put("content", "hey you, this works");
+        indexRequestBuilder.setRefresh(true);
+        indexRequestBuilder.setSource(source);
+        indexRequestBuilder.execute().actionGet();
         ResponseEntity<String> response = restTemplate.getForEntity("/EvidenceSharingAPI/evidences/E001", String.class);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
         System.out.println(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
@@ -59,6 +59,7 @@ public class EvidenceSharingElasticSearchControllerIntegrationTest {
     public void tearDown() throws Exception {
         DeleteIndexRequest indexRequest = new DeleteIndexRequest("evidences");
         client.admin().indices().delete(indexRequest).actionGet();
-        node.close();
+        client.close();
+        node.stop();
     }
 }
