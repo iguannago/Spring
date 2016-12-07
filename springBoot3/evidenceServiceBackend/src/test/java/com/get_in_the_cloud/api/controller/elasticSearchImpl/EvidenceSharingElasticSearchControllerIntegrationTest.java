42b3af39d91d81adcb90915b8d2eaa5170b5c634
David Crespo <davidcrespoarroyo@gmail.com>
2016-12-07 12:09:59 +0000

    Configured integration test for the POST service to Elastic Search. Configured .gitignore. Configured pom.xml with Elastic Search dependencies for the integration tests.

--- springBoot3/evidenceServiceBackend/src/test/java/com/get_in_the_cloud/api/controller/elasticSearchImpl/EvidenceSharingElasticSearchControllerIntegrationTest.java
+++ springBoot3/evidenceServiceBackend/src/test/java/com/get_in_the_cloud/api/controller/elasticSearchImpl/EvidenceSharingElasticSearchControllerIntegrationTest.java
@@ -1,6 +1,13 @@
 package com.get_in_the_cloud.api.controller.elasticSearchImpl;
 
 import com.get_in_the_cloud.api.domain.evidence.Evidence;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +16,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.net.URISyntaxException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,21 +28,37 @@ public class EvidenceSharingElasticSearchControllerIntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+    private final Node node = new NodeBuilder().node().start();
+    private final Client client = node.client();
 
-    @Test
+    @Test@Ignore
     public void getEvidenceEndpointTest() {
-        ResponseEntity<String> response = restTemplate.getForEntity("/EvidenceSharingAPI/evidences/E002", String.class);
+        CreateIndexRequest indexRequest = new CreateIndexRequest("evidences");
+        client.admin().indices().create(indexRequest).actionGet();
+
+        client.admin().indices().prepareTypesExists("evidences").setTypes("evidences").execute();
+
+//        PutMappingResponse response = client.admin().indices().preparePutMapping(schemaName).setSource(mapping)
+//                .setType("evidence").execute().actionGet();
+
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/EvidenceSharingAPI/evidences/E001", String.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
         System.out.println(response);
     }
 
     @Test
-    public void postEvidenceEndpointTest() throws URISyntaxException {
+    public void postEvidenceEndpointTest() {
         ResponseEntity<String> response = restTemplate.postForEntity("/EvidenceSharingAPI/evidences",
-                new Evidence("E98", "some content"), String.class);
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+                new Evidence("E001", "some content"), String.class);
         System.out.println(response);
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
     }
 
-
+    @After
+    public void tearDown() throws Exception {
+        DeleteIndexRequest indexRequest = new DeleteIndexRequest("evidences");
+        client.admin().indices().delete(indexRequest).actionGet();
+        node.close();
+    }
 }
